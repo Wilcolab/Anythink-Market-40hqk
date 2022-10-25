8f14b127f0e86725856617c5b07626e96178fb76
napzzz <92196093+napzzz@users.noreply.github.com>
2022-10-25 18:45:31 +0000

    Add default item image to api

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -147,6 +147,7 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
 
       item.seller = user;
+      item.image = item.image || "/placeholder.png";
 
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
