112a06db09da24568d489396675506275ef447c9
napzzz <92196093+napzzz@users.noreply.github.com>
2022-10-27 03:21:55 +0000

    Add reveal searchbox

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const revealSearch = () => {
+    setShowSearch(true);
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <SearchBox />
+          <span id="get-part" onClick={revealSearch}>
+            A place to get
+          </span>
+          {showSearch && <SearchBox />}
           <span> the cool stuff.</span>
         </div>
       </div>
